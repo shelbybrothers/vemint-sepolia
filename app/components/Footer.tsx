d42411488522095ebd44410f7bda873caa5e6326
shelbybrothers <100958717+shelbybrothers@users.noreply.github.com>
2025-05-19 12:39:06 +0700

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,21 +3,21 @@ export default function Footer() {
     <div className="z-30 mt-auto flex w-full bg-black p-4 text-sm text-xs text-zinc-400 sm:text-sm md:border-[#5788FA]/50 md:border-t">
       Powered by{' '}
       <a
-        href="https://onchainkit.xyz/"
+        href="https://x.com/VemintProtocol"
         target="_blank"
         rel="noopener noreferrer"
         className="font-bold transition-colors hover:text-zinc-300"
       >
-        OnchainKit
+        Twitter/X
       </a>
       <span className="mx-2">·</span>
       <a
-        href="https://github.com/coinbase/onchain-agent-demo"
+        href="https://t.me/VemintDAO"
         target="_blank"
         rel="noopener noreferrer"
         className="transition-colors hover:text-zinc-300"
       >
-        Fork this template
+        Community
       </a>
     </div>
   );
